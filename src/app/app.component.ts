e784b4618a4cf9f25c1b9af16193d169b12631b8
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-07-16 21:04:36 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmpComponent } from './page/manage-emp/manage-emp.component';
 import { ViewAllEmployeeComponent } from './page/view-all-employee/view-all-employee.component';
+import { NavComponent } from './common/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmpComponent,ViewAllEmployeeComponent],
+  imports: [RouterOutlet,ManageEmpComponent,ViewAllEmployeeComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/page/view-all-employee/view-all-employee.component.ts
+++ src/app/page/view-all-employee/view-all-employee.component.ts
@@ -2,28 +2,77 @@ import { NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from "sweetalert2";
+import { NavComponent } from '../../common/nav/nav.component';
 
 @Component({
   selector: 'app-view-all-employee',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,NgFor],
+  imports: [HttpClientModule, FormsModule, NgFor,NavComponent],
   templateUrl: './view-all-employee.component.html',
   styleUrl: './view-all-employee.component.css'
 })
-export class ViewAllEmployeeComponent{
+export class ViewAllEmployeeComponent {
 
-  public employeeList:any;
+  public employeeList: any;
 
-  constructor(private http:HttpClient){
+  constructor(private http: HttpClient) {
     this.loadEmployeeTable();
   }
 
-  loadEmployeeTable(){
+  loadEmployeeTable() {
     this.http.get("http://localhost:8080/emp-controller/get-all")
-    .subscribe(
-      (response) =>{
-        this.employeeList=response;
+      .subscribe(
+        (response) => {
+          this.employeeList = response;
+        }
+      )
+  }
+
+  deleteEmployee(employee: any) {
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.http.delete(`http://localhost:8080/emp-controller/delete-employee/${employee.id}`, { responseType: 'text' })
+          .subscribe(
+            (response) => {
+              this.loadEmployeeTable();
+              swalWithBootstrapButtons.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+              console.log(response);
+            }
+          )
+
+
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
       }
-    )
+    });
   }
 }
