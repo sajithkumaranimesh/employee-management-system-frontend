121a21127c04c55de673737a9a2ffe64e95b6cf4
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-07-16 15:55:58 +0530

    add employee database

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
@@ -25,8 +26,14 @@ export class ManageEmpComponent {
 
   addEmployee() {
     this.http.post("http://localhost:8080/emp-controller/add-employee",this.employeeObj)
-    .subscribe((data) => {
-      console.log(data);
-    });
+    .subscribe(
+      (data) =>{
+        Swal.fire({
+          title: "Good job!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+      }
+    )
   }
 }
