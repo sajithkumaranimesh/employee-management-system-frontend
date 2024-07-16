1e71630bb72ff4c68c6dc26b3e63d139207e5e3e
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-07-16 12:32:32 +0530

    add employee

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
 export class ManageEmpComponent {
 
+  public employeeObj={
+    fistName:"",
+    lastName:"",
+    email:"",
+    departmentId:"",
+    roleId:""
+  }
+
+
+  constructor(private http:HttpClient){}
+
+  addEmployee() {
+    this.http.post("http://localhost:8080/emp-controller/add-employee",this.employeeObj)
+    .subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
